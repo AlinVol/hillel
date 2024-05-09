0aa64f787e1492db2c59bc05997811bde95b4168
AlinVol <alina.voloshyna@gmail.com>
2024-05-09 17:59:24 +0200

    home work

---
+++ main.py
@@ -0,0 +1,23 @@
+# ДЗ 2.1. Виведення числа в стовпчик
+
+# number = int(input ('Enter 4-digit number: '))
+# n1 = number // 1000
+# n2 = number % 1000 // 100
+# n3 = number % 100 // 10
+# n4 = number % 10
+
+# print(n1)
+# print(n2)
+# print(n3)
+# print(n4)
+
+# ДЗ 2.2. Необхідно "перевернути" 5-ти значне число
+# number = int(input ('Enter 5-digit number: '))
+# n1 = number // 10000
+# n2 = number % 10000 // 1000
+# n3 = number % 1000 // 100
+# n4 = number % 100 // 10
+# n5 = number % 10
+# result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
+# print(result)
+
